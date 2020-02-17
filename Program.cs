bb7e1623fd86ac6281a33c70f07182fe2cb5aa63
Nhan Duong <nhanduong@mail.usf.edu>
2020-02-17 18:34:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is Nhan Duong");
+            int[] myArray = { 1, 2, 3, 4, 5 };
+            for (int i=0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
